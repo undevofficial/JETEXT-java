a4447ddb9ec8d5df0edd35d82d6bfd31ce6cd6f3
UNDEV <42951902+undevofficial@users.noreply.github.com>
2019-03-31 23:34:17 +0530

    Update and rename ApiClient.java to src/main/java/com/jetext/ApiClient.java

--- src/main/java/com/jetext/ApiClient.java
+++ src/main/java/com/jetext/ApiClient.java
@@ -1,4 +1,4 @@
-package com.msg91;
+package com.jetext;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -8,13 +8,13 @@ import org.json.JSONObject;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
-import com.msg91.exceptions.InvalidRequestException;
-import com.msg91.exceptions.Msg91Exception;
+import com.jetext.exceptions.InvalidRequestException;
+import com.jetext.exceptions.jetextException;
 
 public class ApiClient {
 
   private String authkey;
-  private static final String API_BASE_PATH = "http://test.panel.msg91.com/api";
+  private static final String API_BASE_PATH = "http://control.jetext.com";
   private MessageFactory messageFactory;
 
   public String getAuthkey() {
@@ -26,7 +26,7 @@ public class ApiClient {
     this.messageFactory = MessageFactory.getInstance(this);
   }
 
-  public Long getRouteBalance(MessageRoute route) throws Msg91Exception {
+  public Long getRouteBalance(MessageRoute route) throws jetextException {
     Map<String, Object> parameters = new HashMap<String, Object>();
     parameters.put("type", route.getValue());
     ApiResponse response = this.request("balance.php", parameters);
@@ -38,7 +38,7 @@ public class ApiClient {
     throw new InvalidRequestException(response.getJson().getObject().getString("msg"));
   }
 
-  public Boolean isValid() throws Msg91Exception {
+  public Boolean isValid() throws jetextException {
     ApiResponse response = this.request("validate.php");
 
     if (response.isJson()) {
@@ -49,12 +49,12 @@ public class ApiClient {
     throw new InvalidRequestException(response.getRaw());
   }
 
-  public ApiResponse request(String endpoint) throws Msg91Exception {
+  public ApiResponse request(String endpoint) throws jetextException {
     Map<String, Object> parameters = new HashMap<String, Object>();
     return this.request(endpoint, parameters);
   }
 
-  public ApiResponse request(String endpoint, Map<String, Object> parameters) throws Msg91Exception {
+  public ApiResponse request(String endpoint, Map<String, Object> parameters) throws jetextException {
     String url = API_BASE_PATH + "/" + endpoint;
 
     try {
@@ -63,7 +63,7 @@ public class ApiClient {
 
       return new ApiResponse(response.getBody());
     } catch (UnirestException e) {
-      throw new Msg91Exception(e.getMessage());
+      throw new jetextException(e.getMessage());
     }
   }
 
